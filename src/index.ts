3891002ad9c1d4c68f614131c7d1abee67d79141
KenFang <kenfangch@yahoo.com.tw>
2021-02-25 19:17:08 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("aaSDxx");
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(200);
+linkedList.add(-20);
+linkedList.add(-300);
+linkedList.add(2500);
+linkedList.sort();
+linkedList.print();
